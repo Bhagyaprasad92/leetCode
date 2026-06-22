8959e12dd45ee67d1ffd03e385e67f4b368265a4
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-06-22 16:39:38 +0530

    Time: 7 ms (51.86%), Space: 47.6 MB (50.4%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,18 +1,22 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
         int totalLength = 0;
-        Map<Character, Integer> cfreq = new HashMap<>();
+        // Map<Character, Integer> cfreq = new HashMap<>();
+        int[] cfreq = new int[26];
         for(char c : chars.toCharArray()) {
-            cfreq.put(c, cfreq.getOrDefault(c, 0)+1);
+            // cfreq.put(c, cfreq.getOrDefault(c, 0)+1);
+            cfreq[c - 'a']++;
         }
         for(int i = 0; i < words.length; i++) {
-            Map<Character, Integer> wfreq = new HashMap<>();
+            // Map<Character, Integer> wfreq = new HashMap<>();
+            int[] wfreq = new int[26];
             for(char c : words[i].toCharArray()) {
-                wfreq.put(c, wfreq.getOrDefault(c, 0)+1);
+                // wfreq.put(c, wfreq.getOrDefault(c, 0)+1);
+                wfreq[c - 'a']++;
             }
             boolean isValid = true;
-            for(char c : wfreq.keySet()) {
-                if(wfreq.get(c) > cfreq.getOrDefault(c, 0)) {
+            for(int j = 0; j < 26; j++) {
+                if(wfreq[j] > cfreq[j]) {
                     isValid = false;
                     break;
                 }
