904a6efcd4575cb69e9ff46b303a51b4f15375e1
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-06-23 09:58:21 +0530

    Time: 0 ms (100%), Space: 42.9 MB (71.97%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
@@ -1,17 +1,17 @@
 class Solution {
     public int rearrangeCharacters(String s, String target) {
         int ans = Integer.MAX_VALUE;
-        int[] sfreq = new int[26];
+        int[] stringfreq = new int[26];
         for(char c : s.toCharArray()) {
-            sfreq[c - 'a']++;
+            stringfreq[c - 'a']++;
         }
-        int[] tfreq = new int[26];
+        int[] targetfreq = new int[26];
         for(char c : target.toCharArray()) {
-            tfreq[c - 'a']++;
+            targetfreq[c - 'a']++;
         }
         for(int i = 0; i < 26; i++) {
-            if(tfreq[i] > 0) {
-                ans = Math.min(ans, sfreq[i] / tfreq[i]);
+            if(targetfreq[i] > 0) {
+                ans = Math.min(ans, stringfreq[i] / targetfreq[i]);
             }
         }
         return ans;
