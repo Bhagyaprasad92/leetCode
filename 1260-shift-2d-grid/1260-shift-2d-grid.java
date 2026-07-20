a6da71bca63cb912117613ae0e9612cf96912055
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-07-20 22:08:38 +0530

    Time: 9 ms (16.09%), Space: 47.1 MB (79.54%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,30 +1,29 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int total = n * m;
-        k %= total;
+        int m = grid.length, n = grid[0].length;
+        int totalKeys = m * n;
+        k %= totalKeys;
         while (k-- > 0) {
-            int last = grid[n - 1][m - 1];
-            for (int i = n - 1; i >= 0; i--) {
-                for (int j = m - 1; j >= 0; j--) {
-                    if(j == 0 && i > 0) {
-                        grid[i][j] = grid[i - 1][m - 1];
-                    } else if(j > 0) {
+            int last = grid[m - 1][n - 1];
+            for (int i = m - 1; i >= 0; i--) {
+                for (int j = n - 1; j >= 0; j--) {
+                    if (j == 0 && i > 0) {
+                        grid[i][j] = grid[i - 1][n - 1];
+                    } else if (j > 0) {
                         grid[i][j] = grid[i][j - 1];
                     }
                 }
             }
             grid[0][0] = last;
         }
-        List<List<Integer>> res = new ArrayList<>();
-        for(int[] row : grid) {
-            List<Integer> innerList = new ArrayList<>();
-            for(int val : row) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int[] row : grid) {
+            ArrayList<Integer> innerList = new ArrayList<>();
+            for (int val : row) {
                 innerList.add(val);
             }
-            res.add(innerList);
+            ans.add(innerList);
         }
-        return res;
+        return ans;
     }
 }
