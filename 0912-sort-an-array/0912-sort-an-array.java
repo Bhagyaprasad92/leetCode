21249e5ea7d353bfaa14cb20fb5e1d9c349850e1
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-07-21 21:10:45 +0530

    Time: 22 ms (81.36%), Space: 80.9 MB (85.9%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,41 +1,31 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length - 1);
+        int[] temp = new int[nums.length];
+        mergeSort(nums, temp, 0, nums.length - 1);
         return nums;
     }
-    private void mergeSort(int[] nums, int l, int r) {
+    private void mergeSort(int[] nums, int[] temp, int l, int r) {
         if(l < r) {
             int mid = l + (r - l) / 2;
-            mergeSort(nums, l, mid);
-            mergeSort(nums, mid + 1, r);
-            merge(nums, l, mid, r);
+            mergeSort(nums, temp, l, mid);
+            mergeSort(nums, temp, mid + 1, r);
+            merge(nums, temp, l, mid, r);
         }
     }
-    private void merge(int[] nums, int l, int m, int r) {
-        int n1 = m - l + 1;
-        int n2 = r - m;
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-        for(int i = 0; i < n1; ++i) {
-            L[i] = nums[l + i];
-        } 
-        for(int i = 0; i < n2; ++i) {
-            R[i] = nums[m + 1 + i];
+    private void merge(int[] nums, int[] temp, int l, int m, int r) {
+        for(int i = l; i <= r; i++) {
+            temp[i] = nums[i];
         }
-        int i = 0, j = 0, k = l;
-        while(i < n1 && j < n2) {
-            if(L[i] <= R[j]) {
-                nums[k] = L[i++];
+        int i = l, j = m + 1, k = l;
+        while(i <= m && j <= r) {
+            if(temp[i] <= temp[j]) {
+                nums[k++] = temp[i++];
             } else {
-                nums[k] = R[j++];
+                nums[k++] = temp[j++];
             }
-            k++;
         }
-        while(i < n1) {
-            nums[k++] = L[i++];
-        }
-        while(j < n2) {
-            nums[k++] = R[j++];
+        while(i <= m) {
+            nums[k++] = temp[i++];
         }
     }
 }
