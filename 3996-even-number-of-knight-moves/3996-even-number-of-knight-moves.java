31d536cefa7c9535599f3ad15b643609adfcae26
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-07-22 07:37:57 +0530

    Time: 0 ms (100%), Space: 43.8 MB (90.05%) - LeetHub

--- 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.java
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean canReach(int[] start, int[] target) {
-        return (start[0] + start[1]) % 2 == (target[0] + target[1]) % 2;
+        boolean res = false;
+        if((start[0] + start[1]) % 2 == (target[0] + target[1]) % 2) {
+            res = true;
+        }
+        return res;
     }
 }
