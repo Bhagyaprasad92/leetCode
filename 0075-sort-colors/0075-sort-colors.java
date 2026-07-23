cce16398112f69091f0419dd909f9b8aa1be8af4
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-07-23 12:07:21 +0530

    Time: 0 ms (100%), Space: 43.8 MB (11.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,39 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int low = 0, mid = 0, high = nums.length - 1;
-        while(mid <= high) {
-            if(nums[mid] == 0) {
-                int temp = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = temp;
-                low++;
-                mid++;
-            } else if(nums[mid] == 1) {
-                mid++;
+    private void merge(int[] nums, int l, int r) {
+        if(l >= r) {
+            return;
+        }
+            int mid = l + (r - l) / 2;
+            merge(nums, l, mid);
+            merge(nums, mid + 1, r);
+            mergeSort(nums, l, mid, r);
+    }
+    private void mergeSort(int[] nums, int l, int m, int r) {
+        int n1 = m - l + 1, n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for(int i = 0; i < n1; i++) {
+            L[i] = nums[l+i];
+        } 
+        for(int i = 0; i < n2; i++) {
+            R[i] = nums[m+1+i];
+        }
+        int i = 0, j = 0, k = l;
+        while(i < n1 && j < n2) {
+            if(L[i] <= R[j]) {
+                nums[k++] = L[i++];
             } else {
-                int temp = nums[high];
-                nums[high] = nums[mid];
-                nums[mid] = temp;
-                high--;
+                nums[k++] = R[j++];
             }
         }
+        while(i < n1) {
+            nums[k++] = L[i++];
+        }
+        while(j < n2) {
+            nums[k++] = R[j++];
+        }
+    }
+    public void sortColors(int[] nums) {
+        merge(nums, 0, nums.length - 1);
     }
 }
