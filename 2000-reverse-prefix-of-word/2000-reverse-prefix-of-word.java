9e0679324431da9df200d6a4594991582ec6248e
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-08-23 16:08:23 +0530

    Time: 1 ms (47.67%), Space: 43.1 MB (55.27%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,15 +1,22 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        if (!word.contains(ch + "")) {
-            return word;
+        int index = 0;
+        for (char c : word.toCharArray()) {
+            if (c == ch) {
+                return reverse(word, index);
+            }
+            index++;
         }
-        int index = word.indexOf(ch);
-        int i = 0, j = index;
+        return word;
+    }
+
+    private String reverse(String word, int k) {
+        int i = 0, j = k;
         StringBuilder res = new StringBuilder();
         for (char c : word.toCharArray()) {
             res.append(c);
         }
-        while(i <= j) {
+        while (i <= j) {
             char c = res.charAt(i);
             res.setCharAt(i, res.charAt(j));
             res.setCharAt(j, c);
@@ -18,20 +25,4 @@ class Solution {
         }
         return res.toString();
     }
-
-    // private String reverse(String word, int k) {
-    //     int i = 0, j = k;
-    //     StringBuilder res = new StringBuilder();
-    //     for (char c : word.toCharArray()) {
-    //         res.append(c);
-    //     }
-    //     while (i <= j) {
-    //         char c = res.charAt(i);
-    //         res.setCharAt(i, res.charAt(j));
-    //         res.setCharAt(j, c);
-    //         i++;
-    //         j--;
-    //     }
-    //     return res.toString();
-    // }
 }
