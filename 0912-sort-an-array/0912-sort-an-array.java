7d1d17b35868db7fa6a3d568fc89862d64341b44
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-08-28 09:00:25 +0530

    Time: 28 ms (40.17%), Space: 83.1 MB (63.6%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,31 +1,43 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int[] temp = new int[nums.length];
-        mergeSort(nums, temp, 0, nums.length - 1);
+        mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
-    private void mergeSort(int[] nums, int[] temp, int l, int r) {
-        if(l < r) {
-            int mid = l + (r - l) / 2;
-            mergeSort(nums, temp, l, mid);
-            mergeSort(nums, temp, mid + 1, r);
-            merge(nums, temp, l, mid, r);
+    private void mergeSort(int[] nums, int low, int high) {
+        if(low < high) {
+            int mid = low + (high - low) / 2;
+            mergeSort(nums, low, mid);
+            mergeSort(nums, mid + 1, high);
+            merge(nums, low, mid, high);
         }
     }
-    private void merge(int[] nums, int[] temp, int l, int m, int r) {
-        for(int i = l; i <= r; i++) {
-            temp[i] = nums[i];
+    private void merge(int[] nums, int low, int mid, int high) {
+        int m = mid - low + 1;
+        int n = high - mid;
+        int[] L = new int[m];
+        int[] R = new int[n];
+        for(int i = 0; i < m; i++) {
+            L[i] = nums[low + i];
         }
-        int i = l, j = m + 1, k = l;
-        while(i <= m && j <= r) {
-            if(temp[i] <= temp[j]) {
-                nums[k++] = temp[i++];
+        for(int i = 0; i < n; i++) {
+            R[i] = nums[mid + 1 + i];
+        }
+        int i = 0, j = 0, k = low;
+        while(i < m && j < n) {
+            if(L[i] <= R[j]) {
+                nums[k] = L[i];
+                i++;
             } else {
-                nums[k++] = temp[j++];
+                nums[k] = R[j];
+                j++;
             }
+            k++;
         }
-        while(i <= m) {
-            nums[k++] = temp[i++];
+        while(i < m) {
+            nums[k++] = L[i++];
+        }
+        while(j < n) {
+            nums[k++] = R[j++];
         }
     }
 }
