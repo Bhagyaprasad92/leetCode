31f7edd5530ddf3f124c3812549b1bc4216f8b35
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-08-30 08:24:36 +0530

    Time: 1 ms (98.36%), Space: 42.5 MB (69.71%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,18 +1,11 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        String s = String.valueOf(n);
-        int max = Integer.MIN_VALUE;
-        for(char c : s.toCharArray()) {
-            if((c - '0') > max) max = c - '0';
+        int sum = 0;
+        while(n > 0) {
+            int d = n % 10;
+            sum += d;
+            n /= 10;
         }
-        int[] freq = new int[s.length() + max];
-        for(char c : s.toCharArray()) {
-            freq[c - '0']++;
-        } 
-        int res = 0;
-        for(int i = 1; i < freq.length; i++) {
-            res += freq[i] * i;
-        }
-        return res;
+        return sum;
     }
 }
