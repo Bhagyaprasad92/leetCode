ff19a1ea504fd2d80ab8613ce7a867ba930058c2
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-09-04 12:47:18 +0530

    Time: 1 ms (99.79%), Space: 46.2 MB (88.3%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -2,33 +2,23 @@ class Solution {
     public int firstStableIndex(int[] nums, int k) {
         if(nums.length == 1) return 0;
         int res = -1;
+        int[] maxPref = new int[nums.length];
+        maxPref[0] = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            maxPref[i] = Math.max(maxPref[i - 1], nums[i]);
+        }
+        int[] minPref = new int[nums.length];
+        minPref[nums.length - 1] = nums[nums.length - 1];
+        for(int i = nums.length - 2; i >= 0; i--) {
+            minPref[i] = Math.min(minPref[i + 1], nums[i]);
+        }
         for(int i = 0; i < nums.length; i++) {
-            int max = findMax(nums, 0, i);
-            int min = findMin(nums, i, nums.length);
-            int currScore = max - min;
+            int currScore = maxPref[i] - minPref[i];
             if(currScore <= k) {
                 res = i;
                 break;
             }
         }
         return res;
-    }
-    private int findMax(int[] nums, int m, int n) {
-        int max = Integer.MIN_VALUE;
-        for(int i = m; i <= n; i++) {
-            if(max < nums[i]) {
-                max = nums[i];
-            }
-        }
-        return max;
-    }
-    private int findMin(int[] nums, int m, int n) {
-        int min = Integer.MAX_VALUE;
-        for(int i = m; i < n; i++) {
-            if(min > nums[i]) {
-                min = nums[i];
-            }
-        }
-        return min;
     }
 }
