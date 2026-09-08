a7557232283e7e44fb19e4a244a4debf24c5e718
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-09-08 12:54:51 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (22.48%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countCommas(int n) {
+        long totalCommas = 0, threshold = 1000;
+        while(n >= threshold) {
+            totalCommas += (n - threshold) + 1;
+            threshold *= 1000;
+        }
+        return (int)totalCommas;
+    }
+}
