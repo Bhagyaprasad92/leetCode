91a5d50224a4a17a9b00590aac61b2a7d0972396
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-09-12 16:45:28 +0530

    Time: 1 ms (68.66%), Space: 45.2 MB (18.21%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -4,7 +4,6 @@ class Solution {
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] == target) {
                 minDist = Math.min(minDist, Math.abs(i - start));
-                if(minDist == 0) return 0;
             }
         }
         return minDist;
